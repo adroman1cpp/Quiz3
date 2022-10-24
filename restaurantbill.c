10317a43ccfb91d66c37f41f294919aa5d2c682d
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-10-23 19:33:08 -0700

    Add files via upload
    Restaurant Bill Outline

---
+++ restaurantbill.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    //declare variables
+
+
+
+    //create random to choose items
+
+
+
+
+    //ask the user for input
+
+
+
+
+    //do the calculations and print
+
+
+
+
+
+}
