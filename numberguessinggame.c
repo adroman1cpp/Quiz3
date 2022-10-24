1170f07e826338bcd37dd57f50c55322aaf323bb
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-10-23 20:24:50 -0700

    Number Guessing Game outline

---
+++ numberguessinggame.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+    //declare variables
+
+
+
+    //ask user for input
+
+
+    //create option for 1
+
+
+
+    //create option for 2
+
+
+
+    //create option for 3
+
+
+}
