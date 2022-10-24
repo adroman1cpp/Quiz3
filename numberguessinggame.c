5c6ed3e59a9bc58f072a8064b4d035be1ff7f2fc
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-10-23 20:47:07 -0700

    added option 2

--- numberguessinggame.c
+++ numberguessinggame.c
@@ -8,46 +8,57 @@ int main(){
     int input = 0;
     char u = 'a';
     int w;
-    int max = 15;
+    int max = 100;
     int error = 0;
 
 
     //ask user for input
     while(input !=3){
 
-    printf("\nPress 1 to play a game");
-    printf("\nPress 2 to change max number");
-    printf("\nPress 3 to quit\n");
-    scanf("%d", &input);
-    srand(time(0));
-    int random = (rand(%max)+1);
+        printf("\nPress 1 to play a game");
+        printf("\nPress 2 to change max number");
+        printf("\nPress 3 to quit\n");
+        scanf("%d", &input);
+        srand(time(0));
+        int random = (rand(%max)+1);
 
-    //create option for 1
-    if(choice==1){
+        //create option for 1
+        if(input==1){
 
-        a:printf("\nEnter your guess: ");
-        scanf("%c",&u);
-        if(u=='u'){
-            continue;
-        } else {
-            w = u- '0';
-            if(w==random) {
-                printf("You have won!"\n);
-            } else if(n>random) {
-                printf{"Your guess was higher."}
-                goto a;
+            a:printf("\nEnter your guess: ");
+            scanf("%c",&u);
+            if(u=='u'){
+                continue;
             } else {
-                printf("Your guess was lower.")
-                goto a;
+                w = u- '0';
+                if(w==random) {
+                    printf("You have won!"\n);
+                } else if(n>random) {
+                    printf{"Your guess was higher."}
+                    goto a;
+                } else {
+                    printf("Your guess was lower.")
+                    goto a;
+                }
             }
         }
-    }
+        //create option for 2
+        else if(input==2) {
 
-    //create option for 2
+            b:printf("\nYou can set Maximum to 100");
+            printf("\nEnter new value: ");
+            scanf("%d", $max);
+            if(max>0 && max<=100){
+                printf("Value has been set.\n");
+            } else {
+                printf("Invalid value.\n");
+                goto b;
+            }
+        }
 
 
 
-    //create option for 3
+        //create option for 3
 
     }   
 
