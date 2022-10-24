fd0f11c5bdfcc552b046ce3703a0c49bef2952f4
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-10-23 19:36:03 -0700

    Add files via upload

--- restaurantbill.c
+++ restaurantbill.c
@@ -3,9 +3,13 @@
 int main() {
 
     //declare variables
-
-
-
+    int salad = 9.95;
+    int soup = 4.55;
+    int sandwich = 13.25;
+    int pizza = 22.35;
+    float tax;
+    float tip;
+    
     //create random to choose items
 
 
